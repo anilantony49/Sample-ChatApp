b00e5024e5f864ebf339aead8fdf7216327fcc51
Anil Antony <anilvithayathil007@gmail.com>
2022-06-24 10:29:14 +0530

    Initial commit-Homescreen created

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+
+import 'package:flutter/material.dart';
+import 'package:whatsapp/my_home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      
+      home: const MyHomePage(),
+      
+    );
+    
+  }
+}
+
+
---
+++ lib/my_home_page.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          actions: [
+             // Widget for the search
+            Tooltip(
+             message: "Search", 
+             child:IconButton(
+              icon:  const Icon(Icons.search),
+              onPressed: (){},)),
+            
+            // Widget for implementing the three-dot menu
+            PopupMenuButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(18),
+              ),
+              itemBuilder: (context) {
+                return [
+                  // In this case, we need 5 popupmenuItems one for each option.
+                  const PopupMenuItem(child: Text('New Group')),
+                  const PopupMenuItem(child: Text('New Broadcast')),
+                  const PopupMenuItem(child: Text('Linked Devices')),
+                  const PopupMenuItem(child: Text('Starred Messages')),
+                  const PopupMenuItem(child: Text('Settings')),
+                ];
+              },
+            ),
+          ],
+          backgroundColor: const Color(0xff128C7E),
+          title: const Text('WhatsApp'),
+          bottom: const TabBar(
+            indicatorSize: TabBarIndicatorSize.tab,
+            indicatorColor: Colors.white,
+            tabs: [
+              Tab(
+                  iconMargin: EdgeInsets.all(100),
+                  child: Icon(
+                    Icons.camera_alt_rounded,
+                  )),
+              Tab(
+                child: Text('CHATS', style: TextStyle(color: Colors.white)),
+              ),
+              Tab(
+                child: Text('STATUS', style: TextStyle(color: Colors.white)),
+              ),
+              Tab(
+                child: Text('CALLS', style: TextStyle(color: Colors.white)),
+              ),
+            ],
+            labelColor: Colors.white,
+          ),
+        ),
+      
+
+        
+        body: const TabBarView(
+          children: [
+            Center(child: Text('This feature is coming soon')),
+            Center(child: Text('This feature is coming soon')),
+            Center(child: Text('This feature is coming soon')),
+            Center(child: Text('This feature is coming soon')),
+            // ChatsTab(),
+            // StatusTab(),
+            // CallTab()
+          ]
+        
+        ),
+      )
+    );
+  }
+}
